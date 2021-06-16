8249f60b1ccef281778b08964422704dd5fdb25b
sb3-spec <68346980+sb3-spec@users.noreply.github.com>
2021-06-16 14:12:21 -0700

    Add files via upload

---
+++ src/Components/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'JavaScript',
+        path: '/javascript',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Java',
+        path: '/java',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Python',
+        path: '/python',
+        cName: 'dropdown-link'
+    },
+];
