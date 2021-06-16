ed8e497239872b78ddf9ee076588a1758f956fb3
sb3-spec <68346980+sb3-spec@users.noreply.github.com>
2021-06-16 13:42:43 -0700

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Footer.css';
+import '../App.css';
+
+function Footer() {
+    return (
+        <div class='wrapper bg-dark'>
+            <div class='container text-center'>
+                <h3>Contact for inquiries!</h3>
+                <ul>
+                    <li>
+                        714-381-5418
+                    </li>
+                    <li>
+                        sheffira2@gmail.com
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
